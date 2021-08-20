293c622b6e1b7df26cfa72ef737dce3966a56f6e
abhirampjayan <abhirampjayan@outlook.com>
2021-08-20 14:13:18 +0530

    created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Board from "./board";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <h1 className="heading">Tic Tac Toe</h1>
+        <div className="game">
+          <div className="game-board">
+            <Board />
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
